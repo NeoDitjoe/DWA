ccad9dee98b88397241d2b88c26c2cac44832b55
NeoDitjoe <ditjoeneo03@gmail.com>
2023-06-08 16:17:23 +0200

    DWA6

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -5,19 +5,23 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-
-  if (divider === '' || dividend === ''){
-    result.innerText = 'Division not performed. Both values are required in inputs. Try again'
-  }
-  else if (dividend && divider < 0){
-    result.innerText = 'Division not performed. Invalid number provided. Try again'
-    console.error('Division not performed. Invalid number provided. Try again')
-  }
-  else if (isNaN(divider) || isNaN(dividend)){
-    console.error('Something critical went wrong. Please reload the page')
-    document.querySelector('body').innerText = 'Something critical went wrong. Please reload the page'
-  }
-  else{
-    result.innerText = parseInt(dividend / divider);
+  try {
+    
+    if (divider === '' || dividend === ''){
+      result.innerText = 'Division not performed. Both values are required in inputs. Try again'
+    }
+    else if (dividend < 0 || divider < 0){
+      result.innerText = 'Division not performed. Invalid number provided. Try again'
+      throw 'Division not performed. Invalid number provided. Try again'
+    }
+    else if (isNaN(divider) || isNaN(dividend)){
+      document.querySelector('body').innerText = 'Something critical went wrong. Please reload the page'
+      throw 'Something critical went wrong. Please reload the page'
+    }
+    else{
+      result.innerText = parseInt(dividend / divider);
+    }
+  } catch (error) {
+    console.error(new Error(error))
   }
 });
